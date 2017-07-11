c2dc7372006e69c2801f83c001da0edc4a4cb56e
FanTaSyLin <fanlin19830908@hotmail.com>
2017-07-11 14:06:22 +0800

    增加了GIIR LMI两仪器的文件获取逻辑

--- FY4ASGS-Monitor/routers/lv1fastview.js
+++ FY4ASGS-Monitor/routers/lv1fastview.js
@@ -23,7 +23,9 @@
     };
 
     function _downloadfastview(req, res, next) {
-        // FY4A-_AGRI--_N_DISK_0995E_L1A_GEO-_C001_NUL_20170426081500_20170426082750_4000M_00000_AFN20170426081500.JPG
+	    // FY4A-_AGRI--_N_DISK_1047E_L1A_GEO-_C001_NUL_20170426081500_4000M_00000_AFN20170426081500.JPG
+	    // FY4A-_GIIRS-_N_REGX_1047E_L1A_GEO-_VIS-_NUL_20170705202236_2000M_00000_GLS20170705202236.JPG
+	    // FY4A-_LMI---_N_LAMA_1047E_L1A_GPL-_SING_NUL_20170706070400_7800M_00000_LMV20170706070400.JPG
 	    console.log(req.url);
         var satID = req.params['satid'];
         var inst = req.params['inst'];
@@ -40,12 +42,25 @@
         while (inst.length < 6) {
             inst += '-';
         }
+        while (chnl.length < 4) {
+            chnl += '-';
+        }
         console.log(inst);
-        var filePath = satID + '_' + inst + '_' + 'N_DISK_1047E_L1A_GEO-'
-            + '_' + chnl + '_' + 'NUL' + '_' + bTime + '_' +
-            resolution + '_' + '00000' + '_' + taskID + '.JPG';
-
-        var filePath = path.join('/shinetek/fy4mcs', filePath);
+	    var filePath = '';
+	    if (inst === 'AGRI--') {
+		    filePath = satID + '_' + inst + '_' + 'N_DISK_1047E_L1A_GEO-'
+			    + '_' + chnl + '_' + 'NUL' + '_' + bTime + '_' +
+			    resolution + '_' + '00000' + '_' + taskID + '.JPG';
+	    } else if (inst === 'GIIRS-') {
+             filePath = satID + '_' + inst + '_' + 'N_REGX_1047E_L1A_GEO-'
+			    + '_' + 'VIS-' + '_' + 'NUL' + '_' + bTime + '_' +
+			    '2000M' + '_' + '00000' + '_' + taskID + '.JPG';
+	    } else if (inst === 'LMI---') {
+            filePath = satID + '_' + inst + '_' + 'N_LAMA_1047E_L1A_GPL-'
+			    + '_' + 'SING' + '_' + 'NUL' + '_' + bTime + '_' +
+			    '7800M' + '_' + '00000' + '_' + taskID + '.JPG';
+	    }
+        filePath = path.join('/shinetek/fy4mcs', filePath);
         console.log(filePath);
         fs.exists(filePath, function (isExistFlg) {
             console.log(isExistFlg);
